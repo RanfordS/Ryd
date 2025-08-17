84cf571d9bc8d13fadfcf546f3bd9ddd6aa44b1a
RanfordS <ranfordsaisamy@live.com>
2025-08-17 14:51:58 +0100

    Added utility functions for prototype Lua parser

---
+++ proto_source/utils.lua
@@ -0,0 +1,134 @@
+U = {}
+
+local universal_comp_weight = {
+    ["nil"] = 0,
+    boolean = 1,
+    number = 2,
+    string = 3,
+    table = 4,
+}
+
+---Universal comparison function, intended for use in `table.sort`.
+---Note, relies on `tostring` for most types, may be slow.
+---@param lhs any Left hand side.
+---@param rhs any Right hand side.
+---@return boolean result Whether `lhs < rhs` is considered true.
+function U.universal_comp (lhs, rhs)
+    local t_lhs = type(lhs)
+    local t_rhs = type(rhs)
+    if t_lhs ~= t_rhs then
+        return universal_comp_weight[t_lhs]
+            <  universal_comp_weight[t_rhs]
+    end
+
+    local t = t_lhs
+    if t ~= "number" and t ~= "string" then
+        lhs = tostring(lhs)
+        rhs = tostring(rhs)
+    end
+    return lhs < rhs
+end
+
+---Gets a list of all the keys in a given table.
+---Note that the order maybe be inconsistent between runs.
+---@param tab table
+---@return table
+function table.list_keys (tab)
+    local keys = {}
+    for key, _ in pairs (tab) do
+        keys[#keys+1] = key
+    end
+    return keys
+end
+
+---Takes a table and returns another table where all of the input table values
+---are keys with `true` as the value. Useful when checking for the presence of
+---many table values.
+---@param tab table Table to get values from.
+---@return {[any]: true} map Resulting value map.
+function table.value_map (tab)
+    local map = {}
+    for _, v in pairs(tab) do
+        map[v] = true
+    end
+    return map
+end
+
+---Returns the key of an item within a given table.
+---If the item occurs within the table multiple times then there is no guarantee
+---which key will be returned.
+---If the table does not contain the item, `nil` is returned.
+---@param tab table Table to search within.
+---@param item any Item to search for.
+---@return any? key Matching key, or `nil` if the value is not found.
+function table.key_of (tab, item)
+    for k, v in pairs(tab) do
+        if v == item then
+            return k
+        end
+    end
+    return nil
+end
+
+---Returns whether a particular item is in the given table.
+---@param tab table Table to search within
+---@param item any Item to search for.
+---@return boolean present True when the table contains the item.
+function table.contains (tab, item)
+    return table.key_of(tab, item) ~= nil
+end
+
+function string.escape (str)
+    local new = str
+    :gsub("\\", "\\\\")
+    :gsub("\n", "\\n")
+    :gsub("\t", "\\t")
+    :gsub("\"", "\\\"")
+    return '"'.. new ..'"'
+end
+
+local function indent (str, depth)
+    return ("    "):rep(depth) .. str
+end
+
+local splat_supported_key_types = {
+    number = true,
+    string = true,
+    boolean = true,
+}
+
+---@param tab table
+---@param depth integer
+---@return string
+local function splat (tab, depth)
+    local keys = table.list_keys(tab)
+    local lines = {"{"}
+    table.sort(keys, U.universal_comp)
+    for _, key in ipairs(keys) do
+        local value = tab[key]
+        local t_key = type(key)
+        --TODO: this may be an unreasonable constraint, re-evaluate
+        assert(splat_supported_key_types[t_key],
+            ("`%s` is not a supported key type for `splat`"):format(t_key))
+
+        local t_value = type(value)
+        local str_value
+        if t_value == "table" then
+            str_value = splat(value, depth + 1)
+        elseif t_value == "string" then
+            str_value = value:escape()
+        else
+            str_value = tostring(value)
+        end
+        local line = indent(tostring(key) .." = ".. str_value ..",", depth + 1)
+        lines[#lines+1] = line
+    end
+    lines[#lines+1] = indent("}", depth)
+    return table.concat(lines, "\n")
+end
+
+
+function table.splat (tab)
+    return splat(tab, 0)
+end
+
