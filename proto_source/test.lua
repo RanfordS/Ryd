f3a959e02e6b2e203cc9358fddd782fc2c7dc066
RanfordS <ranfordsaisamy@live.com>
2025-08-18 17:57:35 +0100

    Added additional functionality to the testing framework

--- proto_source/test.lua
+++ proto_source/test.lua
@@ -1,3 +1,11 @@
 require "utils_test"
 require "ryd_tokenizer_test"
 
+--[[
+Expect_Error("check_for_error-pass", function() error("Some error") end)
+Expect_Error("check_for_error-fail", function() return "Not error"  end)
+--]]
+
+print("")
+Test_Summary()
+
--- proto_source/test_framework.lua
+++ proto_source/test_framework.lua
@@ -3,14 +3,17 @@
 --  2.  The resulting string is written to a file in [m|test_output/].
 --  3.  It is also compared with the corresponding file in [m|test_baseline/].
 --  4.  If the files match then the test passes, otherwise it fails.
---  5.  When a test fails, investigate the result by performing a diff.
---  6.  If the new output file is correct, use it to replace the baseline.
+--  When a test fails, you can investigate the result by performing a diff
+--  between output and the baseline.
+--  If the new output file is correct, replace the baseline.
 
 local test_results = {}
 
 local pass = "\27[1;32mPass\27[0m"
 local fail = "\27[1;31mFail\27[0m"
 
+---@param test_name string Unique name for the test.
+---@param actual string Resulting output of the tested code.
 function Test_Result (test_name, actual)
     assert(test_results[test_name] == nil,
         ("Multiple test have the name `%s`"):format(test_name))
@@ -37,5 +40,48 @@ function Test_Result (test_name, actual)
     test_results[test_name] = result
 
     print((result and pass or fail) .." - ".. test_name)
+end
+
+---@param test_name string Unique name for the test.
+---@param f fun(...: any): any Function to be tested.
+---@param ... any Optional arguments to pass to the function.
+function Expect_Error (test_name, f, ...)
+    assert(test_results[test_name] == nil,
+        ("Multiple test have the name `%s`"):format(test_name))
+    local success, _ = pcall(f, ...)
+    local result = not success
+    test_results[test_name] = result
+
+    print((result and pass or fail) .." - ".. test_name)
+end
+
+function Test_Summary ()
+    local passed, failed = 0, 0
+    ---@type string[]
+    local failed_tests = {}
+
+    for test, result in pairs(test_results) do
+        if result then
+            passed = passed + 1
+        else
+            failed_tests[#failed_tests+1] = test
+            failed = failed + 1
+        end
+    end
+
+    print(("\27[1;32mPassed:\27[0m %i"):format(passed))
+    if failed == 0 then
+        print("\27[1;32mAll passed\27[0m")
+    else
+        print(("\27[1;31mFailed:\27[0m %i"):format(failed))
+
+        print("")
+        print("\27[1mFailed tests:\27[0m")
+        table.sort(failed_tests)
+        for _, test in ipairs(failed_tests) do
+            print("- ".. test)
+        end
+    end
+
 end
 
