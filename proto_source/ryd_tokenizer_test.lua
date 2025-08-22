06e0ce943f132814e1ab0d102e7d4b150830c4f0
RanfordS <ranfordsaisamy@live.com>
2025-08-22 16:08:14 +0100

    Change list and table syntax for unambiguous nesting

--- proto_source/ryd_tokenizer_test.lua
+++ proto_source/ryd_tokenizer_test.lua
@@ -15,17 +15,17 @@ local complete_input = [=[
 
 The following text is an example of many [Ryd] features being used at once.
 As a quick overview, these are:
-[list]
+[list|
 |   Invariant commands, such as the [m|[l]list[r]] start and [m|[l]Ryd[r]] logo.
 |   Argument commands, such as [m|[l]section[|]<name>[r]].
 |   Free-floating separators.
 |   Groups, such as the table alignment list for the table.
-[end]
+]
 
 [section|Special Commands]
 
 The following commands can be used to include the special characters literally.
-[table|{cc|l}]
+[table|{cc|l}|
 | Character | Command | Description |[nr]
 [h-line]
 | [l]       | [m|[l]l[r]] | Left square bracket. |[nr]
@@ -33,13 +33,14 @@ The following commands can be used to include the special characters literally.
 | [|]       | [m|[l][|][r]] | Vertical bar/pipe. |[nr]
 | [{]       | [m|[l][{][r]] | Left curly brace. |[nr]
 | [}]       | [m|[l][}][r]] | Right curly brace. |[nr]
-[end]
+]
 ]=]
 print("")
 print(">> Complete Test")
 local tokenized = R.tokenize(complete_input)
 local complete_result = table.splat(tokenized)
-print(complete_result)
+--print(complete_result)
+print(R.token_list_to_string(complete_input, tokenized, 0))
 Test_Result("ryd.tokenizer-large_complete_sample.lua",
     "return ".. complete_result)
 
